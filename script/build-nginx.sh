4cf0ebccd35d88bd19594afe0a2683f248b6ac39
Florent H. CARRÉ <colundrum@users.noreply.github.com>
2018-01-28 15:18:41 +0100

    revert to fixed version due to stupid dev about tag version

--- script/build-nginx.sh
+++ script/build-nginx.sh
@@ -6,18 +6,18 @@ PATCH_PATH=$HOME/nginx.spec.centos${CENTOS_MAJOR_VERSION}.patch
 
 NGINX_VERSION=$(grep '^+Version:' $PATCH_PATH | cut -d ' ' -f 2)
 
-HEADERS_MORE_VERSION=$(curl --silent https://api.github.com/repos/openresty/headers-more-nginx-module/git/refs/tags | jq -r '.|reverse|.[].ref' | grep -v 'rc' | head -1 | cut -d '/' -f3)
-HTTP_ACCOUNTING_MODULE_VERSION=$(curl --silent https://api.github.com/repos/Lax/ngx_http_accounting_module/git/refs/tags | jq -r '.|reverse|.[].ref' | grep -v 'rc' | head -1 | cut -d '/' -f3)
-HTTP_AUTH_PAM_VERSION=$(curl --silent https://api.github.com/repos/sto/ngx_http_auth_pam_module/git/refs/tags | jq -r '.|reverse|.[].ref' | grep -v 'rc' | head -1 | cut -d '/' -f3)
-HTTP_CACHE_PURGE_VERSION=$(curl --silent https://api.github.com/repos/FRiCKLE/ngx_cache_purge/git/refs/tags | jq -r '.|reverse|.[].ref' | grep -v 'rc' | head -1 | cut -d '/' -f3)
-HTTP_FANCYINDEX_VERSION=$(curl --silent https://api.github.com/repos/aperezdc/ngx-fancyindex/git/refs/tags | jq -r '.|reverse|.[].ref' | grep -v 'rc' | head -1 | cut -d '/' -f3)
-HTTP_INTERNAL_REDIRECT_VERSION=$(curl --silent https://api.github.com/repos/flygoast/ngx_http_internal_redirect/git/refs/tags | jq -r '.|reverse|.[].ref' | grep -v 'rc' | head -1 | cut -d '/' -f3)
-HTTP_UPLOADPROGRESS_VERSION=$(curl --silent https://api.github.com/repos/masterzen/nginx-upload-progress-module/git/refs/tags | jq -r '.|reverse|.[].ref' | grep -v 'rc' | head -1 | cut -d '/' -f3)
-NAXSI_VERSION=$(curl --silent https://api.github.com/repos/nbs-system/naxsi/git/refs/tags | jq -r '.|reverse|.[].ref' | grep -v 'rc' | head -1 | cut -d '/' -f3)
-NCHAN_VERSION=$(curl --silent https://api.github.com/repos/slact/nchan/git/refs/tags | jq -r '.|reverse|.[].ref' | grep -v 'rc' | head -1 | cut -d '/' -f3)
-NGINX_OPENSSL_VERSION=$(curl --silent https://api.github.com/repos/apcera/nginx-openssl-version/git/refs/tags | jq -r '.|reverse|.[].ref' | grep -v 'rc' | head -1 | cut -d '/' -f3)
-NGINX_PUSH_STREAM_VERSION=$(curl --silent https://api.github.com/repos/wandenberg/nginx-push-stream-module/git/refs/tags | jq -r '.|reverse|.[].ref' | grep -v 'rc' | head -1 | cut -d '/' -f3)
-NGINX_VOD_VERSION=$(curl --silent https://api.github.com/repos/kaltura/nginx-vod-module/git/refs/tags | jq -r '.|reverse|.[].ref' | grep -v 'rc' | head -1 | cut -d '/' -f3)
+HEADERS_MORE_VERSION=0.33
+HTTP_ACCOUNTING_MODULE_VERSION=0.5
+HTTP_AUTH_PAM_VERSION=1.5.1
+HTTP_CACHE_PURGE_VERSION=2.3
+HTTP_FANCYINDEX_VERSION=0.4.2
+HTTP_INTERNAL_REDIRECT_VERSION=0.6
+HTTP_UPLOADPROGRESS_VERSION=0.9.2
+NAXSI_VERSION=0.55.3
+NCHAN_VERSION=1.1.14
+NGINX_OPENSSL_VERSION=0.04
+NGINX_PUSH_STREAM_VERSION=0.5.4
+NGINX_VOD_VERSION=1.22
 
 cat <<EOS > /shared/modules_version.md
 
